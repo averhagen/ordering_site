ea221b69ee2d578fb7d5be3c46ac406ab26b6410
amverhagen <averhagen@users.noreply.github.com>
2018-06-20 23:38:18 -0400

    Create model for 'Order' table.

--- ordering/models.py
+++ ordering/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length = 200)    
+    name = models.CharField(max_length = 200)
+
+class Order(models.Model):
+    date_ordered = models.DateTimeField('date ordered')   
