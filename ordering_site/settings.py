bdbf3fc7fcfb94242c30ce49fd259772d0930293
amverhagen <averhagen@users.noreply.github.com>
2018-06-20 23:24:12 -0400

    Create model and migration for 'Product' model.

--- ordering_site/settings.py
+++ ordering_site/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ordering.app.OrderingConfig'
+    'ordering.apps.OrderingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
