312821c2fa649574dde2e05a48c0bc9956e6cb60
amverhagen <averhagen@users.noreply.github.com>
2018-06-23 16:33:48 -0400

    Register 'user' model in admin.py

--- ordering/admin.py
+++ ordering/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Order, Product
+from .models import Order, Product, User
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Product)
+admin.site.register(User)
