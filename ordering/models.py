dbecdc82380f906f37603bde736976f6675fe0b9
amverhagen <averhagen@users.noreply.github.com>
2018-06-23 16:40:58 -0400

    Override __str__ methods in 'user', 'product', and 'order' models.

--- ordering/models.py
+++ ordering/models.py
@@ -4,8 +4,18 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length = 200)
 
+    def __str__(self):
+        return self.name        
+
 class Order(models.Model):
     date_ordered = models.DateTimeField('date ordered')
 
+    def __str__(self):
+        return id + " " + self.date_ordered
+
+
 class User(models.Model):
     email = models.EmailField(unique = True)
+
+    def __str__(self):
+        return self.email
