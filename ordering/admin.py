b5b3264ed8facae837cc0c5f8059d0c4657b5485
amverhagen <averhagen@users.noreply.github.com>
2018-06-24 17:08:37 -0400

    Register 'store' model with admin page.

--- ordering/admin.py
+++ ordering/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Order, Product, User
+from .models import Order, Product, User, Store
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Product)
 admin.site.register(User)
+admin.site.register(Store)
