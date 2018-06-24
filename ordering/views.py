0aac83d54765360160e5d5dc7febd4e512ae8e82
amverhagen <averhagen@users.noreply.github.com>
2018-06-24 17:14:55 -0400

    Change index in 'views.py' to lookup and pass a store to index view based on input param

--- ordering/views.py
+++ ordering/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Store
+
 # Create your views here.
-def index(request):
-    return render(request, 'ordering/index.html', {})
+def index(request, store_identifier):
+    store = get_object_or_404(Store, url_identifying_name = store_identifier)
+    context = {'store': store}
+    return render(request, 'ordering/index.html', context)
