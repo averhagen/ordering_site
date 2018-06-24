c0ab6ce2bb24d1c960499b30dd813592f2438ff9
amverhagen <averhagen@users.noreply.github.com>
2018-06-24 16:24:24 -0400

    Create model for 'store' and make migrations.

--- ordering/models.py
+++ ordering/models.py
@@ -19,3 +19,10 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Store(models.Model):
+    name = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
