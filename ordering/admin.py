78fcf0aa74b02f0038b4cfe984fcb7974fcc1331
amverhagen <averhagen@users.noreply.github.com>
2018-06-26 16:43:40 -0400

    Register 'StoreMenuOption' model with admin.py.

--- ordering/admin.py
+++ ordering/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Order, Product, User, Store
+from .models import Order, Product, User, Store, StoreMenuOption
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Product)
 admin.site.register(User)
 admin.site.register(Store)
+admin.site.register(StoreMenuOption)
