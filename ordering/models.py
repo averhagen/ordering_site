41d704cd8cb92ae6afa82778ce26e713521b96cf
amverhagen <averhagen@users.noreply.github.com>
2018-06-26 17:04:53 -0400

    Change 'StoreMenuOption' model __str__ method to return display_name.

--- ordering/models.py
+++ ordering/models.py
@@ -35,3 +35,6 @@ class StoreMenuOption(models.Model):
     display_name = models.CharField(max_length = 20)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     display_order = models.IntegerField()
+
+    def __str__(self):
+        return self.display_name
