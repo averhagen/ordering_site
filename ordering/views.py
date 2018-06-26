b6cfcc67bf38bbe59707859fddbc5dab35a479fe
amverhagen <averhagen@users.noreply.github.com>
2018-06-26 15:43:58 -0400

    Pass in menuchoices in index context

--- ordering/views.py
+++ ordering/views.py
@@ -6,5 +6,5 @@ from .models import Store
 # Create your views here.
 def index(request, store_identifier):
     store = get_object_or_404(Store, url_identifying_name = store_identifier)
-    context = {'store': store}
+    context = {'store': store, 'menuchoices': ['a', 'b', 'c']}
     return render(request, 'ordering/index.html', context)
