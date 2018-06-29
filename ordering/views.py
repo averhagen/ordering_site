6b4e3ed1010cfb69a4ef40c1d2b2f94a8fb4b926
amverhagen <averhagen@users.noreply.github.com>
2018-06-29 18:11:53 -0400

    Change the index view function to look for a category based on params and also pass a category to index.html.

--- ordering/views.py
+++ ordering/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
-from .models import Store
+from .models import Store, StoreCategory
 
 # Create your views here.
-def index(request, store_identifier):
-    store = get_object_or_404(Store, url_identifying_name = store_identifier)
-    context = {'store': store, 'menuchoices': ['a', 'b', 'c']}
+
+
+def index(request, store_identifier, store_category_id=''):
+    store = get_object_or_404(Store, url_identifying_name=store_identifier)
+    category = None
+    try:
+        category = StoreCategory.objects.get(pk=store_category_id)
+    except StoreCategory.DoesNotExist:
+        pass
+    context = {'store': store, 'category': category}
     return render(request, 'ordering/index.html', context)
