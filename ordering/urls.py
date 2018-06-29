1d1b4424cc336552b81aad7f22e93fd414c1917d
amverhagen <averhagen@users.noreply.github.com>
2018-06-29 18:10:55 -0400

    Change the index url to handle an integer param, used to locate the store_category selected.

--- ordering/urls.py
+++ ordering/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
+
+app_name = 'ordering'
+
 urlpatterns = [
-    path('<store_identifier>', views.index, name = 'index' )
+    path('<store_identifier>/', views.index, name = 'index' ),
+    path('<store_identifier>/<int:store_category_id>', views.index, name = 'index')
 ]
