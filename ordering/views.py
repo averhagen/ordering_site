2e84aa4a50a6d4f55f1cb9868c3d41b424024c6d
amverhagen <averhagen@users.noreply.github.com>
2018-06-30 10:46:04 -0400

    Add a check in ordering.views.index that makes sure the passed category param belongs to the parent store.

--- ordering/views.py
+++ ordering/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
 
 from .models import Store, StoreCategory
 
@@ -9,9 +8,11 @@ from .models import Store, StoreCategory
 def index(request, store_identifier, store_category_id=''):
     store = get_object_or_404(Store, url_identifying_name=store_identifier)
     category = None
-    if(isinstance(store_category_id, int)):    
+    if isinstance(store_category_id, int):
         try:
             category = StoreCategory.objects.get(pk=store_category_id)
+            if category.store != store:
+                category = None
         except StoreCategory.DoesNotExist:
             pass
     context = {'store': store, 'category': category}
