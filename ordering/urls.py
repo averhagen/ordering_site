6fe900587dc833c7e3ee810a9d29a040388da064
amverhagen <averhagen@users.noreply.github.com>
2018-08-08 22:15:56 -0400

    Create a url catch for add_to_cart.

--- ordering/urls.py
+++ ordering/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'ordering'
 
 urlpatterns = [
-    path('<store_identifier>/', views.index, name = 'index' ),
-    path('<store_identifier>/<int:store_category_id>', views.index, name = 'index')
+    path('<store_identifier>/<int:store_category_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('<store_identifier>/', views.index, name='index'),
+    path('<store_identifier>/<int:store_category_id>', views.index, name='index'),
 ]
