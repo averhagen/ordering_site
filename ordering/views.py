e4d1023507bc706e922ece21e29da38bc3960361
amverhagen <averhagen@users.noreply.github.com>
2018-08-20 19:33:11 -0400

    Change add_to_cart to redirect to index if an error occurs.

--- ordering/views.py
+++ ordering/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.urls import urlpatterns
+
 from .models import Store, Category, OrderProduct, User
 
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
@@ -18,13 +21,15 @@ def index(request, store_identifier, store_category_id=''):
     category = Category.find_category_given_category_pk_and_store_pk(
         category_pk=store_category_id, store_pk=store_result.id)
 
-    products_in_cart = OrderProduct.get_products_in_cart_for_user(user_pk=1)
+    products_in_cart = OrderProduct.get_products_in_cart_for_user(
+        user_pk=1)
 
     context = {'store': store_result, 'category': category,
                'products_in_cart': products_in_cart}
     return render(request, 'ordering/index.html', context)
 
 
+@login_required
 def add_to_cart(request, store_id, store_category_id):
     logging.debug('add_to_cart called')
     logging.debug(request.POST)
@@ -32,6 +37,10 @@ def add_to_cart(request, store_id, store_category_id):
     store_result = get_object_or_404(
         Store, id=store_id)
 
-    User.objects.get(pk=1).add_product_to_cart(request.POST['product_to_add'])
+    try:
+        product_to_add_id = request.POST['product_to_add']
+        User.objects.get(pk=1).add_product_to_cart(product_to_add_id)
+    except Exception as e:
+        pass
 
     return HttpResponseRedirect(reverse('ordering:index', args=(store_result.url_identifying_name, store_category_id)))
