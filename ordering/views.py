4bad5b4e6321c3b5c3cbb542254eee25c81931e6
amverhagen <averhagen@users.noreply.github.com>
2018-08-20 12:30:24 -0400

    Change add_to_cart function to add a product to cart.

--- ordering/views.py
+++ ordering/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Store, Category, OrderProduct
+from .models import Store, Category, OrderProduct, User
 
 logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
@@ -20,11 +20,18 @@ def index(request, store_identifier, store_category_id=''):
 
     products_in_cart = OrderProduct.get_products_in_cart_for_user(user_pk=1)
 
-    context = {'store': store_result, 'category': category, 'products_in_cart': products_in_cart}
+    context = {'store': store_result, 'category': category,
+               'products_in_cart': products_in_cart}
     return render(request, 'ordering/index.html', context)
 
 
-def add_to_cart(request, store_identifier, store_category_id):
+def add_to_cart(request, store_id, store_category_id):
     logging.debug('add_to_cart called')
     logging.debug(request.POST)
-    return HttpResponseRedirect(reverse('ordering:index', args=(store_identifier, store_category_id)))
+
+    store_result = get_object_or_404(
+        Store, id=store_id)
+
+    User.objects.get(pk=1).add_product_to_cart(request.POST['product_to_add'])
+
+    return HttpResponseRedirect(reverse('ordering:index', args=(store_result.url_identifying_name, store_category_id)))
